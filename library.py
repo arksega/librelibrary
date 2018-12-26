adb5a4dcae9033417684fc5154203a79b6e419d4
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2018-12-26 11:19:10 -0600

    Initial commit

---
+++ library.py
@@ -0,0 +1,23 @@
+from flask import (Flask, render_template, send_from_directory)
+import os
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def home():
+    '''
+    :return:    the rendered template 'home.html'
+    '''
+    return render_template('home.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+               os.path.join(app.root_path, 'static'),
+               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
