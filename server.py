a923afcd5d66f2f360d5f28469c155d48a202185
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2019-01-11 00:48:15 -0600

    Adding favicon endpoint

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 import connexion
+import os
 
 app = connexion.FlaskApp(__name__, specification_dir='./')
 app.add_api('swagger.yml', strict_validation=True, validate_responses=True)
@@ -7,10 +8,14 @@ app.add_api('swagger.yml', strict_validation=True, validate_responses=True)
 
 @app.route('/')
 def home():
-    '''
-    :return:    the rendered template 'home.html'
-    '''
     return render_template('home.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == '__main__':
